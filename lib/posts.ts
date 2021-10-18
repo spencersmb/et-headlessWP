4cb9069979c40fbf527c491f437a8c430075d99d
Spencer Bigum <spencer.bigum@gmail.com>
2021-10-17 21:17:29 -0400

    removed bad code

--- lib/posts.ts
+++ lib/posts.ts
@@ -1,20 +1,24 @@
-import { getApolloClient, initializeApollo } from './apollo-client'
+import { initializeApollo } from './apollo-client'
 import { QUERY_ALL_POSTS, QUERY_POST_PER_PAGE } from '../graphqlData/postsData'
 
+const getApolloClient = async () => {
+
+}
 /**
  * getAllPosts
  */
 export async function getAllPosts():Promise<{posts: IPost[]}> {
-  const apolloClient = getApolloClient();
+  // const apolloClient = getApolloClient();
+  //
+  // const data = await apolloClient.query({
+  //   query: QUERY_ALL_POSTS,
+  // });
 
-  const data = await apolloClient.query({
-    query: QUERY_ALL_POSTS,
-  });
-
-  const posts = data?.data.posts.edges.map(({ node = {} }) => node);
+  // const posts = data?.data.posts.edges.map(({ node = {} }) => node);
 
   return {
-    posts: Array.isArray(posts) && posts.map(mapPostData),
+    // posts: Array.isArray(posts) && posts.map(mapPostData),
+    posts: []
   };
 }
 
@@ -61,26 +65,26 @@ export function mapPostData(post:IPostRaw | {} = {}): IPost {
 
 }
 
-export async function getPaginatedPosts(currentPage = 1): Promise<IPaginate> {
-  const { posts } = await getAllPosts();
-  const postsPerPage = await getPostsPerPage();
-  const pagesCount = await getPagesCount(posts, postsPerPage);
-
-  let page = Number(currentPage);
-  if (typeof page === 'undefined' || isNaN(page) || page > pagesCount) {
-    page = 1;
-  }
-
-  const offset = postsPerPage * (page - 1);
-  const sortedPosts = sortStickyPosts(posts);
-  return {
-    posts: sortedPosts.slice(offset, offset + postsPerPage),
-    pagination: {
-      currentPage: page,
-      pagesCount,
-    },
-  };
-}
+// export async function getPaginatedPosts(currentPage = 1): Promise<IPaginate> {
+//   const { posts } = await getAllPosts();
+//   const postsPerPage = await getPostsPerPage();
+//   const pagesCount = await getPagesCount(posts, postsPerPage);
+//
+//   let page = Number(currentPage);
+//   if (typeof page === 'undefined' || isNaN(page) || page > pagesCount) {
+//     page = 1;
+//   }
+//
+//   const offset = postsPerPage * (page - 1);
+//   const sortedPosts = sortStickyPosts(posts);
+//   return {
+//     posts: sortedPosts.slice(offset, offset + postsPerPage),
+//     pagination: {
+//       currentPage: page,
+//       pagesCount,
+//     },
+//   };
+// }
 
 export async function getPaginatedPostsV2(currentPage = 1) {
   const apolloClient = initializeApollo();
@@ -99,25 +103,25 @@ export async function getPaginatedPostsV2(currentPage = 1) {
 
 }
 
-export async function getPagesCount(posts: IPost[], postsPerPage = null):Promise<number> {
-  const _postsPerPage = postsPerPage ?? (await getPostsPerPage());
-  return Math.ceil(posts.length / _postsPerPage);
-}
+// export async function getPagesCount(posts: IPost[], postsPerPage = null):Promise<number> {
+//   const _postsPerPage = postsPerPage ?? (await getPostsPerPage());
+//   return Math.ceil(posts.length / _postsPerPage);
+// }
 
-export async function getPostsPerPage(): Promise<number> {
-
-  try {
-    const apolloClient = getApolloClient();
-    const { data } = await apolloClient.query({
-      query: QUERY_POST_PER_PAGE,
-    });
-
-    return Number(data.allSettings.readingSettingsPostsPerPage);
-  } catch (e) {
-    console.log(`Failed to query post per page data: ${e.message}`);
-    throw e;
-  }
-}
+// export async function getPostsPerPage(): Promise<number> {
+//
+//   try {
+//     const apolloClient = getApolloClient();
+//     const { data } = await apolloClient.query({
+//       query: QUERY_POST_PER_PAGE,
+//     });
+//
+//     return Number(data.allSettings.readingSettingsPostsPerPage);
+//   } catch (e) {
+//     console.log(`Failed to query post per page data: ${e.message}`);
+//     throw e;
+//   }
+// }
 
 export async function getPostsPerPageV2(): Promise<number> {
 
