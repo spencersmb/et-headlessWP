6d68d60429f82a97eb999c657480d94ea7115ac5
Spencer Bigum <spencer.bigum@gmail.com>
2021-11-16 20:24:12 -0500

    slug blocking

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -58,7 +58,7 @@ export async function getStaticPaths(context){
 
   return{
     paths: params,
-    fallback: false
+    fallback: "blocking"
   }
 }
 export async function getStaticProps(context){
